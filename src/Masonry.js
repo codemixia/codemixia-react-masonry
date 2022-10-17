823548f5afa6f3b3ea42a227a1161ac2abb9db24
Junsang.Hwang <joontop@linecorp.com>
2022-10-17 13:32:08 +0900

    first commit

---
+++ src/Masonry.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classnames from 'classnames';
+import { autobind } from 'core-decorators';
+
+export default class Masonry extends React.Component {
+  render() {
+    return <div>Hello Masonry</div>;
+  }
+}
